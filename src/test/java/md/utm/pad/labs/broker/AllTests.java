ce5dc37b07e75dfedab5f5801744239793b74cb1
Andrei Rosca <andrei.rosca@ati.utm.md>
2017-09-16 22:16:57 +0300

    Added AllTests and UnitTests tests suites.

---
+++ src/test/java/md/utm/pad/labs/broker/AllTests.java
@@ -0,0 +1,14 @@
+package md.utm.pad.labs.broker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	UnitTests.class,
+	IntegrationTests.class
+})
+public class AllTests {
+
+}
---
+++ src/test/java/md/utm/pad/labs/broker/UnitTests.java
@@ -0,0 +1,14 @@
+package md.utm.pad.labs.broker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	MessageTest.class,
+	ClientHandlerImplTest.class
+})
+public class UnitTests {
+
+}
