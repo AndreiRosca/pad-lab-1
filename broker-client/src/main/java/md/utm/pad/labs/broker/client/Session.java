0b5a944168e7694e20394ae6eac4565f3f99a66d
Andrei Rosca <andrei.rosca@ati.utm.md>
2017-09-17 11:14:20 +0300

    Added the possibility to receive messages using the Session class.

--- broker-client/src/main/java/md/utm/pad/labs/broker/client/Session.java
+++ broker-client/src/main/java/md/utm/pad/labs/broker/client/Session.java
@@ -1,6 +1,8 @@
 package md.utm.pad.labs.broker.client;
 
+import md.utm.pad.labs.broker.ClientChannel;
 import md.utm.pad.labs.broker.Message;
+import md.utm.pad.labs.broker.ReceiveMessageResponse;
 import md.utm.pad.labs.broker.Request;
 import md.utm.pad.labs.broker.client.service.JsonService;
 
@@ -32,5 +34,31 @@ public class Session {
 	public void sendMessage(Queue queue, Message message) {
 		Request request = new Request("send", queue.getName(), message.getPayload());
 		connection.getClientChannel().write(jsonService.toJson(request));
+	}
+
+	public Message receiveMessage() {
+		return receiveMessageFromQueue("");
+	}
+
+	private Message receiveMessageFromQueue(String queueName) {
+		Request request = new Request("receive", queueName);
+		connection.getClientChannel().write(jsonService.toJson(request));
+		String jsonResponse = readResponse();
+		ReceiveMessageResponse response = jsonService.fromJson(jsonResponse, ReceiveMessageResponse.class);
+		return response.getMessage();
+	}
+
+	private String readResponse() {
+		ClientChannel channel = connection.getClientChannel();
+		StringBuilder payload = new StringBuilder();
+		String line;
+		while ((line = channel.readLine()).trim().length() > 0) {
+			payload.append(line);
+		}
+		return payload.toString();
+	}
+
+	public Message receiveMessage(Queue queue) {
+		return receiveMessageFromQueue(queue.getName());
 	}
 }
--- broker-client/src/test/java/md/utm/pad/labs/broker/client/SessionTest.java
+++ broker-client/src/test/java/md/utm/pad/labs/broker/client/SessionTest.java
@@ -2,6 +2,7 @@ package md.utm.pad.labs.broker.client;
 
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,10 +12,12 @@ import org.junit.Test;
 
 import md.utm.pad.labs.broker.ClientChannel;
 import md.utm.pad.labs.broker.Message;
+import md.utm.pad.labs.broker.ReceiveMessageResponse;
 import md.utm.pad.labs.broker.Request;
 import md.utm.pad.labs.broker.client.service.JsonService;
 
 public class SessionTest {
+	private static final String GET_MESSAGE_RESPONSE = "{\"type\":\"response\",\"payload\":\"success\",\"message\":{\"properties\":[],\"payload\":\"Test\"}}";
 
 	Connection connection = mock(Connection.class);
 	JsonService jsonService = mock(JsonService.class);
@@ -24,6 +27,9 @@ public class SessionTest {
 	@Before
 	public void setUp() {
 		when(connection.getClientChannel()).thenReturn(channel);
+		when(channel.readLine()).thenReturn(GET_MESSAGE_RESPONSE, System.lineSeparator());
+		when(jsonService.fromJson(anyString(), eq(ReceiveMessageResponse.class)))
+				.thenReturn(new ReceiveMessageResponse("response", "success", new Message("Test")));
 		session = new Session(connection, jsonService);
 	}
 
@@ -50,5 +56,21 @@ public class SessionTest {
 		session.sendMessage(queue, message);
 		verify(jsonService).toJson(new Request("send", queue.getName(), "Hello"));
 		verify(channel).write(anyString());
+	}
+
+	@Test
+	public void canReceiveMessagesFromTheDefaultQueue() {
+		Message message = session.receiveMessage();
+		verify(jsonService).toJson(new Request("receive", ""));
+		verify(jsonService).fromJson(anyString(), eq(ReceiveMessageResponse.class));
+		assertEquals(new Message("Test"), message);
+	}
+
+	@Test
+	public void canReceiveMessagesFromExistingQueues() {
+		Message message = session.receiveMessage(new Queue("EM_TEST.Q"));
+		verify(jsonService).toJson(new Request("receive", "EM_TEST.Q"));
+		verify(jsonService).fromJson(anyString(), eq(ReceiveMessageResponse.class));
+		assertEquals(new Message("Test"), message);
 	}
 }
