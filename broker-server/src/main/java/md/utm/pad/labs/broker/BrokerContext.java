97c2704384e219dc61816a2103676898fec7f542
Andrei Rosca <anrosca@endava.com>
2017-09-19 12:20:09 +0300

    Improved the BrokerContext class by writing more unit tests and cleaning the existing ones.

--- broker-server/src/main/java/md/utm/pad/labs/broker/BrokerContext.java
+++ broker-server/src/main/java/md/utm/pad/labs/broker/BrokerContext.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import md.utm.pad.labs.broker.subscriber.Subscriber;
 
@@ -26,8 +27,16 @@ public class BrokerContext {
 		}
 	}
 
-	public void sendMulticastMessage(String queueNamePattern, Message message) {
-		Pattern pattern = Pattern.compile(queueNamePattern);
+	public void sendMulticastMessage(String queueNamePattern, Message message) throws InvalidQueueNamePatternException {
+		try {
+			Pattern pattern = Pattern.compile(queueNamePattern);
+			sendMessageToMatchedQueues(pattern, message);
+		} catch (PatternSyntaxException e) {
+			throw new InvalidQueueNamePatternException(e);
+		}
+	}
+
+	private void sendMessageToMatchedQueues(Pattern pattern, Message message) {
 		queues.forEach((queueName, messageQueue) -> {
 			Matcher matcher = pattern.matcher(queueName);
 			if (matcher.matches()) {
@@ -36,21 +45,27 @@ public class BrokerContext {
 		});
 	}
 
-	public void sendMessage(String queueName, Message message) {
+	public void sendMessage(String queueName, Message message) throws NullMessageException, UnknownQueueException {
+		if (message == null)
+			throw new NullMessageException();
+		if (!queueExists(queueName))
+			throw new UnknownQueueException();
 		queues.getOrDefault(queueName, queues.get(DEFAULT_QUEUE_NAME)).addMessage(message);
 		publishMessageToSubscribers(queueName, message);
 	}
 
 	public void sendMessage(Message message) {
-		sendMessage("", message);
+		sendMessage(DEFAULT_QUEUE_NAME, message);
 	}
 
-	public Message receiveMessage(String queueName) {
+	public Message receiveMessage(String queueName) throws UnknownQueueException {
+		if (!queueExists(queueName))
+			throw new UnknownQueueException();
 		return queues.getOrDefault(queueName, queues.get(DEFAULT_QUEUE_NAME)).receiveMessage();
 	}
 
 	public Message receiveMessage() {
-		return receiveMessage("");
+		return receiveMessage(DEFAULT_QUEUE_NAME);
 	}
 
 	public void createQueue(String queueName) throws InvalidQueueNameException {
@@ -72,6 +87,17 @@ public class BrokerContext {
 		return getQueueDepth("");
 	}
 
+	public void registerSubscriber(String queueName, Subscriber subscriber)
+			throws NullSubscriberException, UnknownQueueException {
+		if (subscriber == null)
+			throw new NullSubscriberException();
+		if (!queueExists(queueName))
+			throw new UnknownQueueException();
+		if (!subscribers.containsKey(queueName))
+			subscribers.put(queueName, new CopyOnWriteArraySet<>());
+		subscribers.get(queueName).add(subscriber);
+	}
+
 	public static class InvalidQueueNameException extends RuntimeException {
 		private static final long serialVersionUID = 1L;
 
@@ -83,9 +109,63 @@ public class BrokerContext {
 		}
 	}
 
-	public void registerSubscriber(String queueName, Subscriber subscriber) {
-		if (!subscribers.containsKey(queueName))
-			subscribers.put(queueName, new CopyOnWriteArraySet<>());
-		subscribers.get(queueName).add(subscriber);
+	public static class InvalidQueueNamePatternException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public InvalidQueueNamePatternException() {
+		}
+
+		public InvalidQueueNamePatternException(Exception e) {
+			super(e);
+		}
+
+		public InvalidQueueNamePatternException(String message) {
+			super(message);
+		}
+	}
+
+	public static class NullMessageException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public NullMessageException() {
+		}
+
+		public NullMessageException(Exception e) {
+			super(e);
+		}
+
+		public NullMessageException(String message) {
+			super(message);
+		}
+	}
+
+	public static class NullSubscriberException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public NullSubscriberException() {
+		}
+
+		public NullSubscriberException(Exception e) {
+			super(e);
+		}
+
+		public NullSubscriberException(String message) {
+			super(message);
+		}
+	}
+
+	public static class UnknownQueueException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+
+		public UnknownQueueException() {
+		}
+
+		public UnknownQueueException(Exception e) {
+			super(e);
+		}
+
+		public UnknownQueueException(String message) {
+			super(message);
+		}
 	}
 }
