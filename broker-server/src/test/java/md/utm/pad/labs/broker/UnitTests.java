d44a9fa355abc8d4052285e0fe1997205d0b0f7d
Andrei Rosca <anrosca@endava.com>
2017-09-19 12:45:38 +0300

    Added the SubscriberTest class to the unit-tests suite.

--- broker-server/src/test/java/md/utm/pad/labs/broker/UnitTests.java
+++ broker-server/src/test/java/md/utm/pad/labs/broker/UnitTests.java
@@ -4,11 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import md.utm.pad.labs.broker.subscriber.SubscriberTest;
+
 @RunWith(Suite.class)
 @SuiteClasses({
 	MessageTest.class,
 	ClientHandlerImplTest.class,
-	BrokerContextTest.class
+	BrokerContextTest.class,
+	SubscriberTest.class
 })
 public class UnitTests {
 
