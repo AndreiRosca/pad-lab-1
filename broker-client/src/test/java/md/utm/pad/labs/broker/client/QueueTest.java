a92679971b3e62ece5667693580a705791301a37
Andrei Rosca <anrosca@endava.com>
2017-09-19 13:33:14 +0300

    Implemented the QueueTest class

---
+++ broker-client/src/test/java/md/utm/pad/labs/broker/client/QueueTest.java
@@ -0,0 +1,26 @@
+package md.utm.pad.labs.broker.client;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+public class QueueTest {
+
+	Session session = mock(Session.class);
+
+	@Test
+	public void canGetTheNameOfTheNewlyCreatedQueue() {
+		Queue queue = new Queue(session, "EM_TEST.Q");
+		assertEquals("EM_TEST.Q", queue.getName());
+	}
+
+	@Test
+	public void whenRegisteringAListenerThoughAQueue_TheListenerIsSentToSession() {
+		Queue queue = new Queue(session, "EM_TEST.Q");
+		MessageListener listener = mock(MessageListener.class);
+		queue.setMessageListener(listener);
+		verify(session).registerSubscriber("EM_TEST.Q", listener);
+	}
+}
