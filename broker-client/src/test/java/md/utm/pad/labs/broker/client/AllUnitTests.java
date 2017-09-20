30566dcf6fed75e77b53e53e722300f0b1a464c7
Andrei Rosca <anrosca@endava.com>
2017-09-20 14:29:26 +0300

    Updated the AllUnitTests test suite.

---
+++ broker-client/src/test/java/md/utm/pad/labs/broker/client/AllUnitTests.java
@@ -0,0 +1,15 @@
+package md.utm.pad.labs.broker.client;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	ConnectionTest.class,
+	QueueTest.class,
+	SessionTest.class
+})
+public class AllUnitTests {
+
+}
