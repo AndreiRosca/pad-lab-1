57a6f90a9c2971ec08f7aa458ec53feb4d81443d
Andrei Rosca <andrei.rosca@ati.utm.md>
2017-09-24 08:36:43 +0300

    Now we store the db credentials in a properties file.

--- broker-server/src/main/java/md/utm/pad/labs/broker/DefaultClientHandlerFactory.java
+++ broker-server/src/main/java/md/utm/pad/labs/broker/DefaultClientHandlerFactory.java
@@ -1,5 +1,9 @@
 package md.utm.pad.labs.broker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import md.utm.pad.labs.broker.repository.DataSourceProperties;
 import md.utm.pad.labs.broker.repository.JdbcMessageRepository;
 import md.utm.pad.labs.broker.service.DefaultJsonService;
@@ -14,12 +18,26 @@ public class DefaultClientHandlerFactory implements ClientHandlerFactory {
 	}
 
 	private DataSourceProperties createDataSourceProperties() {
+		try {
+			return tryCreateDataSourceProperties();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private DataSourceProperties tryCreateDataSourceProperties() throws IOException {
+		Properties properties = new Properties();
+		properties.load(getInputStream());
 		return DataSourceProperties.newBuilder()
-				.setDriverClassName("org.postgresql.Driver")
-				.setUrl("jdbc:postgresql://localhost:5432/simple_message_broker")
-				.setUsername("postgres")
-				.setPassword("admin")
+				.setDriverClassName(properties.getProperty("jdbc.driverClassName"))
+				.setUrl(properties.getProperty("jdbc.url"))
+				.setUsername(properties.getProperty("jdbc.username"))
+				.setPassword(properties.getProperty("jdbc.password"))
 				.build();
+	}
+
+	private InputStream getInputStream() {
+		return getClass().getResourceAsStream("/dataSource.properties");
 	}
 
 	@Override
